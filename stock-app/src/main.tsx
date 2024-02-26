a7a196996ad05744774317c4cc6af7b10bf4cc46
Lilach E <lilachelayza@gmail.com>
2024-02-26 19:12:27 +0200

    router fixes

--- stock-app/src/main.tsx
+++ stock-app/src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import { RouterProvider } from "react-router";
 import { router } from "./router.tsx";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- stock-app/src/router.tsx
+++ stock-app/src/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import { App } from "./App";
-import TestComponent from "./TestComponent";
 
 export const router = createBrowserRouter([
   {
@@ -10,10 +9,6 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <p>Main Screen Here</p>,
-      },
-      {
-        path: "/newItem",
-        Component: TestComponent,
       },
     ],
   },
