8b66d6b208fee5cc65455a8e3bc05b6adb31619e
Lilach E <lilachelayza@gmail.com>
2024-02-26 19:03:59 +0200

    router finishes

--- stock-app/src/router.tsx
+++ stock-app/src/router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import TestComponent from "./TestComponent";
 
 export const router = createBrowserRouter([
   {
@@ -9,6 +10,10 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <p>Main Screen Here</p>,
+      },
+      {
+        path: "/newItem",
+        Component: TestComponent,
       },
     ],
   },
