14c6f6323f986738c30efccbb5784e186bb336a2
shai-benari <s.benari1962@gmail.com>
2024-02-26 19:03:17 +0200

    list

---
+++ stock-app/src/list-items.tsx
@@ -0,0 +1,24 @@
+export function ListItemes() {
+    return (
+      <main>
+        <h1>List items</h1>
+        <div>
+            <h3>kitchen</h3>
+        <ul> 
+            <li>pot </li>
+            <li>skillet </li>
+            <li>kettle </li>
+        </ul>
+        </div>
+        <div>
+            <h3>kitchen</h3>
+        <ul> 
+            <li>pot </li>
+            <li>skillet </li>
+            <li>skillet </li>
+        </ul>
+        </div>
+      </main>
+    );
+  }
+  
